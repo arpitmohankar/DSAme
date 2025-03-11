d8eb9041a7ede710676f2dd512451155b594841d
Arpit Mohankar <arpitmohankar28@gmail.com>
2025-03-11 18:29:49 +0530

    testng2

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ class Main {
         System.out.println("Hello World");
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello World");
+            System.out.println("bello");
         }
 
     }
