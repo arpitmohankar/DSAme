404023212c2c01f9f69111fce150dc280801a2f9
Arpit Mohankar <arpitmohankar28@gmail.com>
2025-03-11 19:14:25 +0530

    testng5

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,32 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World");
         System.out.println("Hello World");
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello World");
             System.out.println("Hi learned to connect github with intellij");
         }
+        try {
+            File obj =new File("test.txt");
+            FileWriter obj1 =new FileWriter(obj);
 
+        obj1.write("Hello World arpit");
+        obj1.close();
+        Scanner scanner= new Scanner(obj);
+        while (scanner.hasNextLine()){
+            System.out.println(scanner.nextLine());
+
+        }
+        scanner.close();
+        }
+        catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
